83824b3f12833f9997af67c98a532d98e91b31f6
Emmanuel-Nkongolo <nkongolo64@gmail.com>
2025-04-29 21:27:08 +0200

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRouter from "./routes/products/index.ts";
 
 const port = 3000;
-
 const app = express();
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
