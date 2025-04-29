20b1ef2cfdd970db63d5b26d422c8ca83a6e28c0
Emmanuel-Nkongolo <nkongolo64@gmail.com>
2025-04-29 20:04:24 +0200

    Products Router setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import productsRouter from "./routes/products/index.ts";
 
 const port = 3000;
 
@@ -7,6 +8,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+app.use("/products", productsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ api/src/routes/products/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("The list of products");
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+  res.send("Here is your product ");
+});
+
+router.post("/", (req, res) => {
+  res.send("New product created");
+});
+
+export default router;
